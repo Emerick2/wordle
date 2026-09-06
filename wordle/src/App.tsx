40d626e261885c5afd74c65788c00ff3ac10cd3d
Emerick <pacaudemerick@gmail.com>
2026-09-06 09:48:31 +0200

    Ajout de la possibilitée d'écrire avec le clavier phisique

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -1,13 +1,38 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css'
 import KeyboardEvent from "./Keyboard";
 
 function App() {
   const [keyboardActivated, setKeyboardActivated] = useState<boolean>(true);
 
-  const touche1 : string[] = ["a","z","e","r","t","y","u","i","o","p"]
-  const touche2 : string[] = ["q","s","d","f","g","h","j","k","l","m"]
-  const touche3 : string[] = ["w","x","c","v","b","n"]
+  const touche1 : string[] = ["a","z","e","r","t","y","u","i","o","p"];
+  const touche2 : string[] = ["q","s","d","f","g","h","j","k","l","m"];
+  const touche3 : string[] = ["w","x","c","v","b","n"];
+
+  const allKeys : string[] = [...touche1, ...touche2, ...touche3];
+
+  useEffect(() => {
+    const keyDown = (event : globalThis.KeyboardEvent) => {
+      if (!keyboardActivated) return;
+
+      const key = event.key.toLowerCase();
+
+      if (key === 'enter') {
+        KeyboardEvent({ character: "", deleteButton: false, enterButton: true });
+      }
+      else if (key === 'backspace') {
+        KeyboardEvent({ character: "", deleteButton: true, enterButton: false });
+      }
+      else if (allKeys.includes(key)) {
+        KeyboardEvent({ character: key, deleteButton: false, enterButton: false });
+      }
+    };
+    window.addEventListener('keydown', keyDown);
+
+    return () => {
+      window.removeEventListener("keydown", keyDown);
+    };
+  }, [keyboardActivated]);
 
   return (
     <>    
