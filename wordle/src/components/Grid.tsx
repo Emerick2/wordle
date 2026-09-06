95e6d11280bb35dd1acf15d9e9c1461f19e5645b
Erwan Lievre <lievre.erwan@gmail.com>
2026-09-06 12:07:12 +0200

    récupération mot du jour fonctionnelle et mise au propre des fichiers

--- wordle/src/components/Grid.tsx
+++ wordle/src/components/Grid.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import Attempt from "./Attempt";
 
 export default function Grid() {
+  const [word, setWord] = useState("");
+  const [loading, setLoading] = useState(true);
+
   const attempts = [
     {
       status: "pending" as const,
@@ -63,6 +67,34 @@ export default function Grid() {
       ],
     },
   ];
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/word?lang=fr", {
+      headers: {
+        "x-api-key": "Abc123",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setWord(data.word);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <div className="spinner"></div>
+        <p>Chargement du mot...</p>
+      </div>
+    );
+  }
+
+  console.log(word);
 
   return (
     <section className="grid">
