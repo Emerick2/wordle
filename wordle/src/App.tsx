f5b323a5d40581ad8fc6d85d2d73e1829e913ca6
Emerick <pacaudemerick@gmail.com>
2026-09-07 08:40:44 +0200

    Utilisation de la méthode d'API de Erwan pour résoudre le conflit de fusion

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -130,35 +130,55 @@ function App() {
   }, [keyboardActivated, characterId, lineId, attempts]);
 
 
-    useEffect(() => {
-      const callAPIWordle = async () => {
-        try {
-          const response = await fetch ( 'https://raw.githubusercontent.com/arbxz/wordle-api/main/src/data/words-fr.json');
+    // version 1 de l'API :
+    // useEffect(() => {
+    //   const callAPIWordle = async () => {
+    //     try {
+    //       const response = await fetch ( 'https://raw.githubusercontent.com/arbxz/wordle-api/main/src/data/words-fr.json');
           
-          if (!response.ok){
-            throw new Error(`Erreur l'or de l'appel de l'API : ${response.status}`);
-          }
+    //       if (!response.ok){
+    //         throw new Error(`Erreur l'or de l'appel de l'API : ${response.status}`);
+    //       }
           
-          const data = await response.json();
+    //       const data = await response.json();
           
-          let word = "azerty";
+    //       let word = "azerty";
 
-          if (data != null && data.words != null){
-            word = data.words[RandomInt(0, data.words.length)];
-          }
+    //       if (data != null && data.words != null){
+    //         word = data.words[RandomInt(0, data.words.length)];
+    //       }
 
-          setLoading(false);
-          setWord(word);
-          console.log(word);
-        } catch (erreur) {
-          console.error(`Erreur l'or de l'appel de l'API : ${erreur}`);
-          setLoading(false);
-          setWord("azerty");
-        }
-      };
+    //       setLoading(false);
+    //       setWord(word);
+    //       console.log(word);
+    //     } catch (erreur) {
+    //       console.error(`Erreur l'or de l'appel de l'API : ${erreur}`);
+    //       setLoading(false);
+    //       setWord("azerty");
+    //     }
+    //   };
 
-      callAPIWordle();
-    }, []);
+    //   callAPIWordle();
+    // }, []);
+
+
+  // version 2 de l'API :
+  useEffect(() => {
+    fetch("http://localhost:3000/api/word?lang=fr", {
+      headers: {
+        "x-api-key": "Abc123",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setWord(data.word);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>    
